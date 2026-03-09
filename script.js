9f21fe793347c6f417f6e86e33e3e33cd80116a3
Giuseppe Miroddi <miroddi.giuseppe@gmail.com>
2026-03-09 11:12:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function generateNUmber(){
+    
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
+
+    
+    const result = Math.floor(Math.random() * (max - min + 1) + min);
+    
+    document.getElementById('altroCampo').value = result
+}
